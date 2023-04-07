dec24dcd8f6a98e6a6bc05c7146366a41c967fc4
Seulki-Lim <97998858+limseulki@users.noreply.github.com>
2023-04-07 16:14:54 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,310 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int day = 0;
+        
+        
+        switch(a) {
+            case 1 : 
+                day = b % 7;
+                if(day == 1) {
+                    answer = "FRI";
+                }
+                if(day == 2) {
+                    answer = "SAT";
+                }
+                if(day == 3) {
+                    answer = "SUN";
+                }
+                if(day == 4) {
+                    answer = "MON";
+                }
+                if(day == 5) {
+                    answer = "TUE";
+                }
+                if(day == 6) {
+                    answer = "WED";
+                }
+                if(day == 0) {
+                    answer = "THU";
+                }
+                break;
+                
+            case 2 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "MON";
+                }
+                if(day == 2) {
+                    answer = "TUE";
+                }
+                if(day == 3) {
+                    answer = "WED";
+                }
+                if(day == 4) {
+                    answer = "THU";
+                }
+                if(day == 5) {
+                    answer = "FRI";
+                }
+                if(day == 6) {
+                    answer = "SAT";
+                }
+                if(day == 0) {
+                    answer = "SUN";
+                }
+                break;
+                
+            case 3 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "TUE";
+                }
+                if(day == 2) {
+                    answer = "WED";
+                }
+                if(day == 3) {
+                    answer = "THU";
+                }
+                if(day == 4) {
+                    answer = "FRI";
+                }
+                if(day == 5) {
+                    answer = "SAT";
+                }
+                if(day == 6) {
+                    answer = "SUN";
+                }
+                if(day == 0) {
+                    answer = "MON";
+                }
+                break;
+                
+            case 4 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "FRI";
+                }
+                if(day == 2) {
+                    answer = "SAT";
+                }
+                if(day == 3) {
+                    answer = "SUN";
+                }
+                if(day == 4) {
+                    answer = "MON";
+                }
+                if(day == 5) {
+                    answer = "TUE";
+                }
+                if(day == 6) {
+                    answer = "WED";
+                }
+                if(day == 0) {
+                    answer = "THU";
+                }
+                break;
+                
+            case 5 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "SUN";
+                }
+                if(day == 2) {
+                    answer = "MON";
+                }
+                if(day == 3) {
+                    answer = "TUE";
+                }
+                if(day == 4) {
+                    answer = "WED";
+                }
+                if(day == 5) {
+                    answer = "THU";
+                }
+                if(day == 6) {
+                    answer = "FRI";
+                }
+                if(day == 0) {
+                    answer = "SAT";
+                }
+                break;
+                
+            case 6 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "WED";
+                }
+                if(day == 2) {
+                    answer = "THU";
+                }
+                if(day == 3) {
+                    answer = "FRI";
+                }
+                if(day == 4) {
+                    answer = "SAT";
+                }
+                if(day == 5) {
+                    answer = "SUN";
+                }
+                if(day == 6) {
+                    answer = "MON";
+                }
+                if(day == 0) {
+                    answer = "TUE";
+                }
+                break;
+                
+            case 7 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "FRI";
+                }
+                if(day == 2) {
+                    answer = "SAT";
+                }
+                if(day == 3) {
+                    answer = "SUN";
+                }
+                if(day == 4) {
+                    answer = "MON";
+                }
+                if(day == 5) {
+                    answer = "TUE";
+                }
+                if(day == 6) {
+                    answer = "WED";
+                }
+                if(day == 0) {
+                    answer = "THU";
+                }
+                break;
+                
+            case 8 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "MON";
+                }
+                if(day == 2) {
+                    answer = "TUE";
+                }
+                if(day == 3) {
+                    answer = "WED";
+                }
+                if(day == 4) {
+                    answer = "THU";
+                }
+                if(day == 5) {
+                    answer = "FRI";
+                }
+                if(day == 6) {
+                    answer = "SAT";
+                }
+                if(day == 0) {
+                    answer = "SUN";
+                }
+                break;
+                
+            case 9 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "THU";
+                }
+                if(day == 2) {
+                    answer = "FRI";
+                }
+                if(day == 3) {
+                    answer = "SAT";
+                }
+                if(day == 4) {
+                    answer = "SUN";
+                }
+                if(day == 5) {
+                    answer = "MON";
+                }
+                if(day == 6) {
+                    answer = "TUE";
+                }
+                if(day == 0) {
+                    answer = "WED";
+                }
+                break;
+                
+            case 10 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "SAT";
+                }
+                if(day == 2) {
+                    answer = "SUN";
+                }
+                if(day == 3) {
+                    answer = "MON";
+                }
+                if(day == 4) {
+                    answer = "TUE";
+                }
+                if(day == 5) {
+                    answer = "WED";
+                }
+                if(day == 6) {
+                    answer = "THU";
+                }
+                if(day == 0) {
+                    answer = "FRI";
+                }
+                break;
+                
+            case 11 :
+                day = b % 7;
+                if(day == 1) {
+                    answer = "TUE";
+                }
+                if(day == 2) {
+                    answer = "WED";
+                }
+                if(day == 3) {
+                    answer = "THU";
+                }
+                if(day == 4) {
+                    answer = "FRI";
+                }
+                if(day == 5) {
+                    answer = "SAT";
+                }
+                if(day == 6) {
+                    answer = "SUN";
+                }
+                if(day == 0) {
+                    answer = "MON";
+                }
+                break;
+                
+            case 12 : 
+                day = b % 7;
+                if(day == 1) {
+                    answer = "THU";
+                }
+                if(day == 2) {
+                    answer = "FRI";
+                }
+                if(day == 3) {
+                    answer = "SAT";
+                }
+                if(day == 4) {
+                    answer = "SUN";
+                }
+                if(day == 5) {
+                    answer = "MON";
+                }
+                if(day == 6) {
+                    answer = "TUE";
+                }
+                if(day == 0) {
+                    answer = "WED";
+                }
+                break;
+        }
+        return answer;
+    }
+}
