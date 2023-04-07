67a7eff54be9174bce623ee64a1941d1f7e97288
Seulki-Lim <97998858+limseulki@users.noreply.github.com>
2023-04-07 14:19:15 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int num = x;
+        
+        while(num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        
+        if (x % sum == 0){
+            return answer = true;
+        } else {
+            return answer = false;
+        }
+    }
+}
