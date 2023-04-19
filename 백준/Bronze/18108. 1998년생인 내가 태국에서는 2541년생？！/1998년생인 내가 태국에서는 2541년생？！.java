955c06f9c439d759aa850f3c19ddeb1f131bcc96
Seulki-Lim <97998858+limseulki@users.noreply.github.com>
2023-04-19 15:17:31 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int s = sc.nextInt();
+        int year = s-543;
+        
+        System.out.println(year);
+    }
+}
