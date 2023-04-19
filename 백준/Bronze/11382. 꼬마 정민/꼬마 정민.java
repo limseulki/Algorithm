e68c40c7cfac78d86898234ca138e8a685d42283
Seulki-Lim <97998858+limseulki@users.noreply.github.com>
2023-04-19 15:40:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        long answer = A+B+C;
+        
+        System.out.print(answer);
+    }
+}
