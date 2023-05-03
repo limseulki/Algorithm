3ba8ab849042194ed06a09e3ab38c1dbd058df22
Seulki <97998858+limseulki@users.noreply.github.com>
2023-05-03 21:03:40 +0900

    [Silver IV] Title: 접미사 배열, Time: 188 ms, Memory: 16732 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -1,14 +1,15 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String S = bf.readLine();
+        bf.close();
+
         String[] suffix = new String[S.length()];
 
         for(int i = 0; i < S.length(); i++) {
@@ -16,9 +17,11 @@ public class Main {
         }
 
         Arrays.sort(suffix);
-
+        
         for(int i = 0; i < suffix.length; i++) {
-            System.out.println(suffix[i]);
+            bw.write(suffix[i] + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
