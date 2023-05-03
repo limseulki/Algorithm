ed4ed333bb4aa49faefa58e9481309021a83118b
Seulki <97998858+limseulki@users.noreply.github.com>
2023-05-03 13:16:48 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 19932 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer s = new StringTokenizer(bf.readLine(), " ");
+        System.out.print(s.countTokens());
+    }
+}
