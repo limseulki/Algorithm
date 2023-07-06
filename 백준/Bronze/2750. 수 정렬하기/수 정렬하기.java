3a726d7c3078b323c02350276b9fd6dff9db17bd
Seulki <97998858+limseulki@users.noreply.github.com>
2023-07-06 17:25:32 +0900

    [Bronze II] Title: 수 정렬하기, Time: 184 ms, Memory: 14812 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -8,11 +8,20 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        int temp;
 
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
+
+        sort(N, arr);
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void sort(int N, int[] arr) {
+        int temp;
 
         for(int j = 0; j < N-1; j++) {
             for (int i = 0; i < N-1; i++) {
@@ -22,10 +31,6 @@ public class Main {
                     arr[i + 1] = temp;
                 }
             }
-        }
-
-        for (int i = 0; i < N; i++) {
-            System.out.println(arr[i]);
         }
     }
 }
