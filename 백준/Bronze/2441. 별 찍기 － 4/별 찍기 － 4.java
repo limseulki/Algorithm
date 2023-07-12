a60ae68102a7e5482ba53c902ab7673037ba3d7f
Seulki <97998858+limseulki@users.noreply.github.com>
2023-07-12 23:09:06 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -5,17 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < N; i++) {
             for(int k = 0; k < i; k++) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for(int j = N-i; j > 0; j--) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
