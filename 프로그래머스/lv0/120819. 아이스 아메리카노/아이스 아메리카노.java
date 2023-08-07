f6047aa951c3fe202d0a0ef155103d4ddadcd319
Seulki <97998858+limseulki@users.noreply.github.com>
2023-08-07 11:35:01 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -3,7 +3,7 @@ class Solution {
         int[] answer = new int[2];
         
         answer[0] = money / 5500;
-        answer[1] = money - 5500 * answer[0];
+        answer[1] = money % 5500;
         
         return answer;
     }
