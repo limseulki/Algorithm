1ef141acc09f44e938a6c015f8680812e41b7f7d
Seulki <97998858+limseulki@users.noreply.github.com>
2023-08-09 10:25:55 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -2,11 +2,7 @@ class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         
-        if(n >= 10) {
-            answer = n * 12000 + k * 2000 - (n / 10) * 2000;
-        } else {
-            answer = n * 12000 + k * 2000;
-        }
+        answer = n * 12000 + k * 2000 - (n / 10) * 2000;
         
         return answer;
     }
