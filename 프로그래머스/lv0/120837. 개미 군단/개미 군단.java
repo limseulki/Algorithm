925c7169097dae568bd5e412544254d53cc91c92
Seulki <97998858+limseulki@users.noreply.github.com>
2023-08-10 10:27:44 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -2,18 +2,14 @@ class Solution {
     public int solution(int hp) {
         int answer = 0;
         
-        while(hp > 0) {
-            if(hp >= 5) {
-                answer += hp / 5;
-                hp %= 5;
-            } else if(hp >= 3) {
-                answer += hp / 3;
-                hp %= 3;
-            } else {
-                answer += hp;
-                hp = 0;
-            }
-        }
+        answer += hp / 5;
+        hp %= 5;
+    
+        answer += hp / 3;
+        hp %= 3;
+        
+        answer += hp;
+        
         
         return answer;
     }
