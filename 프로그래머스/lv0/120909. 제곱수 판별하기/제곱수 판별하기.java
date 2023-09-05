713d5714f79bb9e748e01adec999dd484ca735f1
Seulki <97998858+limseulki@users.noreply.github.com>
2023-09-05 17:46:21 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(Math.sqrt(n) % 1 == 0.0) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        
+        return answer;
+    }
+}
