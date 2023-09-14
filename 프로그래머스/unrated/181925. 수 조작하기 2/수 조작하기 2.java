0d7ab341fb1e2e2eec8f34d9b44937a609730303
Seulki <97998858+limseulki@users.noreply.github.com>
2023-09-14 18:52:36 +0900

    [unrated] Title: 수 조작하기 2, Time: 625.68 ms, Memory: 388 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,15 +2,22 @@ class Solution {
     public String solution(int[] numLog) {
         String answer = "";
         
-        for(int i = 0; i < numLog.length-1; i++) {
-            if(numLog[i+1] - numLog[i] == 1) {
-                answer += "w";
-            } else if(numLog[i+1] - numLog[i] == -1) {
-                answer += "s";
-            } else if(numLog[i+1] - numLog[i] == 10) {
-                answer += "d";
-            }  else if(numLog[i+1] - numLog[i] == -10) {
-                answer += "a";
+        for(int i = 1; i < numLog.length; i++){
+            switch(numLog[i] - numLog[i-1]){
+                case 1 :
+                    answer += "w";
+                    break;
+                case -1 :
+                    answer += "s";
+                    break;
+                case -10 :
+                    answer += "a";
+                    break;
+                case 10 :
+                    answer += "d";
+                    break;
+                default :
+                    break;
             }
         }
         
