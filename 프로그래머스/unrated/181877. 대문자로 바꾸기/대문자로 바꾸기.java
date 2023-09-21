6d13d7a8b9659e9d12d8db998181cca5f095fc0a
Seulki <97998858+limseulki@users.noreply.github.com>
2023-09-21 15:52:58 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        answer = myString.toUpperCase();
+        
+        return answer;
+    }
+}
