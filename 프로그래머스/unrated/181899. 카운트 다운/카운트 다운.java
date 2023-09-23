b4967c5cd3d01115faae68a0890b3ea25a53ba10
Seulki <97998858+limseulki@users.noreply.github.com>
2023-09-23 16:23:09 +0900

    [unrated] Title: 카운트 다운, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start-end_num+1];
+        int cnt = 0;
+        
+        for(int i = start; i >= end_num; i--) {
+            answer[cnt] = i;
+            cnt++;
+        }
+        
+        return answer;
+    }
+}
