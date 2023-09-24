908f388cbfb0b8f4ac5b498ef9e8c8a68a1e854f
Seulki <97998858+limseulki@users.noreply.github.com>
2023-09-24 10:12:56 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a % 2 != 0 && b % 2 != 0) {
+            answer = a*a + b*b;
+        } else if(a % 2 != 0 || b % 2 != 0) {
+            answer = 2 * (a + b);
+        } else {
+            answer = Math.abs(a-b);
+        }
+        
+        return answer;
+    }
+}
