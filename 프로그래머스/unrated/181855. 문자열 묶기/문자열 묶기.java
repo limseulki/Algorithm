641809766844a49d7af2575664880f95cdd31a01
Seulki <97998858+limseulki@users.noreply.github.com>
2023-10-02 00:05:46 +0900

    [unrated] Title: 문자열 묶기, Time: 4.49 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(String[] strArr) {
-        int[] count = new int[100000];
-        for(int i = 0 ; i < strArr.length; i++) {
-            count[strArr[i].length()]++;
+        int answer = 0;
+        int[] arr = new int[31];
+
+        for(int i = 0; i < strArr.length; i++) {
+            arr[strArr[i].length()]++;
         }
-        return Arrays.stream(count).max().getAsInt();
+
+        for(int i = 0; i < arr.length; i++) {
+            answer = Math.max(answer, arr[i]);
+        }
+        
+        return answer;
     }
 }
