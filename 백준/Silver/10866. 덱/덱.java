d291ec3355ee03d32ae2d08cd8ba211cb10c336b
Seulki <97998858+limseulki@users.noreply.github.com>
2023-10-21 00:10:57 +0900

    [Silver IV] Title: 덱, Time: 212 ms, Memory: 19148 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayDeque<Integer> dq = new ArrayDeque<Integer>();
+        
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+
+            String[] str = br.readLine().split(" ");
+
+            // 첫 번째 단어에 대한 switch-case 
+            switch (str[0]) {
+
+                case "push_front": {
+                    dq.addFirst(Integer.parseInt(str[1]));
+                    break;
+                }
+
+                case "push_back": {
+                    dq.addLast(Integer.parseInt(str[1]));
+                    break;
+                }
+
+                case "pop_front": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    }
+                    else {
+                        sb.append(dq.pollFirst()).append('\n');
+                    }
+                    break;
+                }
+
+                case "pop_back": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    }
+                    else {
+                        sb.append(dq.pollLast()).append('\n');
+                    }
+                    break;
+                }
+
+                case "size": {
+                    sb.append(dq.size()).append('\n');
+                    break;
+                }
+
+                case "empty": {
+                    if (dq.isEmpty()) {
+                        sb.append(1).append('\n');
+                    }
+                    else {
+                        sb.append(0).append('\n');
+                    }
+                    break;
+                }
+
+                case "front": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    }
+                    else {
+                        sb.append(dq.peekFirst()).append('\n');
+                    }
+                    break;
+                }
+
+                case "back": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    }
+                    else {
+                        sb.append(dq.peekLast()).append('\n');
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
