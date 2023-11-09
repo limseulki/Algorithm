dbf176ab90410921854dac9a82cd3197842f7dad
Seulki <97998858+limseulki@users.noreply.github.com>
2023-11-09 22:56:31 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 13.19 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i = 1; i < food.length; i++) {
+            for(int j = 0; j < food[i]/2; j++) {
+                answer += i;
+            }
+        }
+        
+        answer += 0;
+        
+        for(int i = food.length - 1; i >= 1; i--) {
+            for(int j = 0; j < food[i]/2; j++) {
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
