377cb49081a1a1feea91bf80c3dae8c4d5ae2eaa
Seulki <97998858+limseulki@users.noreply.github.com>
2023-11-16 19:53:07 +0900

    [level unrated] Title: 배열 만들기 6, Time: 18.24 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stkArr = new ArrayList<>();
+        
+        int i = 0;
+        
+        while(i < arr.length) {
+            if(stkArr.size() == 0) {
+                stkArr.add(arr[i]);
+                i++;
+            }
+            else if(stkArr.get(stkArr.size()-1) != arr[i]) {
+                stkArr.add(arr[i]);
+                i++;
+            }
+            else if(stkArr.get(stkArr.size()-1) == arr[i]) {
+                stkArr.remove(stkArr.size()-1);
+                i++;
+            }
+        }
+        
+        if(stkArr.size() == 0) {
+            stkArr.add(-1);
+        }
+        
+        int[] stk = new int[stkArr.size()];
+        
+        for(int j = 0; j < stkArr.size(); j++) {
+            stk[j] = stkArr.get(j);
+        }
+        
+        return stk;
+    }
+}
