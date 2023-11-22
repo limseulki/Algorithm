d97ea33894eb6250e40a6d50be1cc6390ae739a7
Seulki <97998858+limseulki@users.noreply.github.com>
2023-11-22 20:37:59 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(int i = 0; i < d.length; i++) {
+            if(budget < d[i]) {
+                break;
+            }
+            budget -= d[i];
+            answer++;
+        }
+        return answer;
+    }
+}
