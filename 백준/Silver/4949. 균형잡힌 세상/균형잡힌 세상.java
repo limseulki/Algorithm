2252b83622c57cc9ee3f0133521cd5f2eb8d42a9
Seulki <97998858+limseulki@users.noreply.github.com>
2024-01-22 20:38:07 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 564 ms, Memory: 44460 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Stack;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while(true) {
+            String str = br.readLine();
+            if(str.equals(".")) {
+                break;
+            }
+            
+            Stack<String> stack = new Stack<>();
+
+            String[] sentence = str.split("");
+            for(String word : sentence){
+                switch(word) {
+                    case("("):
+                    case("["):
+                        stack.push(word);
+                        break;
+                    case(")"):
+                        if(!stack.isEmpty() && stack.peek().equals("(")) {
+                            stack.pop();
+                        } else {
+                            stack.push(")");
+                        } 
+                        break;
+                    case("]"):
+                        if(!stack.isEmpty() && stack.peek().equals("[")) {
+                            stack.pop();
+                        } else {
+                            stack.push("]");
+                        }
+                        break;
+                }
+            }
+            
+            if(stack.empty()) {
+                bw.write("yes\n");
+            } else {
+                bw.write("no\n");
+            }
+            
+            bw.flush();
+        }
+    }
+}
