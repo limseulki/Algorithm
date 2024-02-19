2ce4bd78f7b555a43b01d6b4eaf67473e2a45a6c
Seulki <97998858+limseulki@users.noreply.github.com>
2024-02-19 22:08:45 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        
+        System.out.print(arr[0] * arr[N-1]);
+    }
+}
