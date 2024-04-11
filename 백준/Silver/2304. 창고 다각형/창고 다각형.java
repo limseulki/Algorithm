bbfa3e856bb1271b41b51e2709de2a0c605dd213
Seulki <97998858+limseulki@users.noreply.github.com>
2024-04-11 21:38:57 +0900

    [Silver II] Title: 창고 다각형, Time: 144 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -0,0 +1,44 @@
+ import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    
+   public static void main(String[] args) throws IOException {
+       
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int N = Integer.parseInt(br.readLine());
+ 
+      int[][] col = new int[N][2];
+       
+      for(int i = 0; i < N; i++) {
+         StringTokenizer st = new StringTokenizer(br.readLine());
+          
+         col[i][0] = Integer.parseInt(st.nextToken());
+         col[i][1] = Integer.parseInt(st.nextToken());
+      }
+ 
+      Arrays.sort(col, (o1, o2) -> o1[0] - o2[0]);
+ 
+      int area = 0;
+       
+      for(int i = 0; i < N;){
+          int j = i + 1;
+          int max = j;
+         while(j < N && col[i][1] > col[j][1]){
+            if(col[max][1] < col[j++][1]) max = j - 1;
+         }
+ 
+         if(j >= N) {
+            area += col[i][1];
+            if(max < N) {
+                area += col[max][1] * (col[max][0] - col[i][0] - 1);
+            }
+            i = max;
+         } else {
+            area += col[i][1] * (col[j][0] - col[i][0]);
+            i = j;
+         }
+      }
+      System.out.println(area);
+   }
+}
