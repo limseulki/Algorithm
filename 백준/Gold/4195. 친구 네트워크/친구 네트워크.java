13cd5659c862ea0ce9888a8344d440473e487cd6
Seulki <97998858+limseulki@users.noreply.github.com>
2024-06-03 19:40:48 +0900

    [Gold II] Title: 친구 네트워크, Time: 1044 ms, Memory: 92964 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	static int[] parents, rank;
+	static int size = 100_001;
+    
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		
+		Map<String, Integer> map = new HashMap<>();
+        
+		for(int test = 0; test < t; test++) {
+			int f = Integer.parseInt(br.readLine());
+			
+			parents = new int[2 * size];
+			rank = new int[2 * size];
+            
+			for(int i = 1; i < 2 * size; i++) {
+				parents[i] = i;
+				rank[i] = 1;
+			}
+			
+			int idx = 1;
+            
+			for(int i = 0; i < f; i++) {
+				String line = br.readLine();
+				st = new StringTokenizer(line);
+                
+				for(int j = 0; j < 2; j++) {
+					String name = st.nextToken();
+					if(!map.containsKey(name)) {
+						map.put(name, idx++);
+					}
+				}
+				
+				String[] network = line.split(" ");
+				int to = map.get(network[0]);
+				int from = map.get(network[1]);
+				int num = union(to, from);
+                
+				sb.append(num + "\n");
+			}
+		}
+        
+		System.out.println(sb.toString());
+	}
+	
+	static int find(int x) {
+		if(parents[x] == x) {
+            return x;
+        }
+        
+		int rx = find(parents[x]);
+        
+		return rx;
+	}
+	
+	static int union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x != y) { 
+			if(x > y) {
+				parents[x] = y;
+				rank[y] += rank[x];
+				return rank[y];
+			} else {
+				parents[y] = x;
+				rank[x] += rank[y];
+			}
+		} 
+		return rank[x];
+	}
+}
